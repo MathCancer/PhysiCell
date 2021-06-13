293f43db61829a5e61fb6428434035e6b7695ffd
Randy Heiland <heiland@indiana.edu>
2021-06-13 09:05:01 -0400

    test build script

---
+++ test_builds.sh
@@ -0,0 +1,31 @@
+# $ make list-projects
+# Sample projects: template2D template3D biorobots-sample cancer-biorobots-sample heterogeneity-sample
+#                  cancer-immune-sample virus-macrophage-sample template pred-prey-farmer worm-sample
+# 
+# Sample intracellular projects: ode1-sample ode2-sample physiboss-cell-lines-sample ecoli-acetic-switch-sample
+
+make
+make reset
+make biorobots-sample 
+make
+make reset
+make cancer-biorobots-sample 
+make
+make reset
+make heterogeneity-sample
+make
+make reset
+make ode1-sample
+make
+make reset
+make ode2-sample
+make
+make reset
+make physiboss-cell-lines-sample 
+make
+make reset
+make ecoli-acetic-switch-sample
+make
+make reset
+make biorobots-sample 
+make
