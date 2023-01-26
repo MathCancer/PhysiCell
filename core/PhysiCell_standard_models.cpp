887040825b16fcbc3094de42c1fd0bb218e9ef30
Juliano <julianoferrarigianlupi@gmail.com>
2023-01-26 18:21:03 -0500

    fixes calcification dynamics

--- core/PhysiCell_standard_models.cpp
+++ core/PhysiCell_standard_models.cpp
@@ -551,7 +551,7 @@ void standard_volume_update_function( Cell* pCell, Phenotype& phenotype, double 
 	phenotype.volume.total = phenotype.volume.cytoplasmic + phenotype.volume.nuclear; 
 	
 	
-	phenotype.volume.fluid_fraction = phenotype.volume.fluid / 
+	phenotype.volume.fluid_fraction += phenotype.volume.fluid / 
 		( 1e-16 + phenotype.volume.total ); 
    
 	phenotype.geometry.update( pCell,phenotype,dt );
