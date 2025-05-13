fe4e810ae1090a942fcce1a42b2ce94340db4f39
Daniel Bergman <danielrbergman@gmail.com>
2025-05-13 16:46:25 -0400

    fix attack no detach
    PR #340 put the correct check in the wrong place to prevent detachment when a cell is attacking another cell.
    This fixes that by first moving the check into the spring attachments function as an attack produces a spring attachment.
    Second, it moves the check into the detachment block rather than the attachment block.

--- core/PhysiCell_standard_models.cpp
+++ core/PhysiCell_standard_models.cpp
@@ -1441,8 +1441,6 @@ void dynamic_attachments( Cell* pCell , Phenotype& phenotype, double dt )
     while( done == false && j < pCell->state.neighbors.size() )
     {
         Cell* pTest = pCell->state.neighbors[j]; 
-		if (phenotype.cell_interactions.pAttackTarget==pTest || pTest->phenotype.cell_interactions.pAttackTarget==pCell) // do not let attackers detach randomly
-		{ continue; }
         if( pTest->state.number_of_attached_cells() < pTest->phenotype.mechanics.maximum_number_of_attachments )
         {
             // std::string search_string = "adhesive affinity to " + pTest->type_name; 
@@ -1469,7 +1467,9 @@ void dynamic_spring_attachments( Cell* pCell , Phenotype& phenotype, double dt )
     double detachment_probability = phenotype.mechanics.detachment_rate * dt; 
     for( int j=0; j < pCell->state.spring_attachments.size(); j++ )
     {
-        Cell* pTest = pCell->state.spring_attachments[j]; 
+        Cell* pTest = pCell->state.spring_attachments[j];
+		if (phenotype.cell_interactions.pAttackTarget==pTest || pTest->phenotype.cell_interactions.pAttackTarget==pCell) // do not let attackers detach randomly
+		{ continue; }
         if( UniformRandom() <= detachment_probability )
         { detach_cells_as_spring( pCell , pTest ); }
     }
